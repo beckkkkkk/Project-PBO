d8cc9519eb93fdbe49423346bd3d2e82a04e7813
klissh <89530841+Desuhairu@users.noreply.github.com>
2024-05-29 10:10:15 +0700

    menambahkan file cuklis

---
+++ Cuklis Ganteng.java
@@ -0,0 +1,8 @@
+public class main{
+
+    public static void main(String[] args){
+        system.out.println("Muhammad Muhibuddin Mukhlish")
+    }
+
+
+}
--- main.java
+++ main.java
@@ -1,7 +1,7 @@
 public class main{
 
 public static void main(String[] args){
-system.out.println("Muhammad Muhibuddin Mukhlish")
+system.out.println("Hello World")
 }
 
 
